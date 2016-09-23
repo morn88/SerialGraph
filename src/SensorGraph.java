ae8afad7c34917acdf778dc2fd97c0029c753181
m0rn <morn88@gmail.com>
2016-09-23 23:53:30 +0300

    minor changes

--- src/SensorGraph.java
+++ src/SensorGraph.java
@@ -7,8 +7,6 @@ import org.jfree.data.xy.XYSeriesCollection;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.Scanner;
 
 /**
